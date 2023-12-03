1b68c9e57db054a5fd178c62644043263a2985fb
Timmy Xiao <34635512+tzx@users.noreply.github.com>
2023-12-03 03:42:18 -0500

    Revert "constexpr ray"
    This reverts commit 6ebdc506e3e448f078bbbeeed5c2a38950e2e49b.

--- src/intersections/ray.cpp
+++ src/intersections/ray.cpp
@@ -1,5 +1,18 @@
 #include "ray.hpp"
 
+Ray::Ray(Tuple origin_, Tuple direction_) {
+  this->orig_ = origin_;
+  this->dir_ = direction_;
+}
+
+const Tuple& Ray::origin() const {
+  return this->orig_;
+}
+
+const Tuple& Ray::direction() const {
+  return this->dir_;
+}
+
 Tuple Ray::position(float t) const {
   return this->origin() + this->direction() * t;
 }
--- src/intersections/ray.hpp
+++ src/intersections/ray.hpp
@@ -5,12 +5,9 @@
 
 class Ray {
   public:
-    constexpr Ray(Tuple origin_, Tuple direction_) :
-      orig_(origin_),
-      dir_(direction_) {}
-
-    constexpr const Tuple& origin() const { return this->orig_; }
-    constexpr const Tuple& direction() const { return this->dir_; }
+    Ray(Tuple origin_, Tuple direction_);
+    const Tuple& origin() const;
+    const Tuple& direction() const;
 
     Tuple position(float t) const;
     Ray transform(const Mat4 &m) const;
